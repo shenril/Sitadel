554101fc93c3ecda53563d4203ee4de3b39185a7
Batard Florent <florent.batard@gmail.com>
2019-08-14 10:59:51 +0900

    #1 Try to parallelize the requests

--- lib/modules/attacks/bruteforce/admin.py
+++ lib/modules/attacks/bruteforce/admin.py
@@ -1,4 +1,5 @@
 from urllib.parse import urljoin
+from concurrent.futures import ThreadPoolExecutor as PoolExecutor
 
 from lib.config.settings import Risk
 from lib.utils.container import Services
@@ -6,27 +7,30 @@ from .. import AttackPlugin
 
 
 class Admin(AttackPlugin):
-    def process(self, start_url, crawled_urls):
-        output = Services.get("output")
-        datastore = Services.get("datastore")
-        request = Services.get("request_factory")
-        logger = Services.get("logger")
+    output = Services.get("output")
+    datastore = Services.get("datastore")
+    request = Services.get("request_factory")
+    logger = Services.get("logger")
 
-        output.info("Checking admin interfaces...")
-        with datastore.open("admin.txt", "r") as db:
+    def check_url(self, url):
+        try:
+            resp = self.request.send(url=url, method="HEAD", payload=None, headers=None)
+            if resp.status_code == 200:
+                if resp.url == url.replace(" ", "%20"):
+                    self.output.finding("Found admin panel at %s" % resp.url)
+        except Exception as e:
+            self.logger.error(e)
+            self.output.error("Error occured\nAborting this attack...\n")
+            self.output.debug("Traceback: %s" % e)
+            return
+
+    def process(self, start_url, crawled_urls):
+        self.output.info("Checking admin interfaces...")
+        with self.datastore.open("admin.txt", "r") as db:
             dbfiles = [x.strip() for x in db.readlines()]
-            try:
-                for adminpath in dbfiles:
-                    url = urljoin(str(start_url), str(adminpath))
-                    output.debug("Testing: %s" % url)
-                    resp = request.send(
-                        url=url, method="HEAD", payload=None, headers=None
-                    )
-                    if resp.status_code == 200:
-                        if resp.url == url.replace(" ", "%20"):
-                            output.finding("Found admin panel at %s" % resp.url)
-            except Exception as e:
-                logger.error(e)
-                output.error("Error occured\nAborting this attack...\n")
-                output.debug("Traceback: %s" % e)
-                return
+            urls = map(
+                lambda adminpath: urljoin(str(start_url), str(adminpath)), dbfiles
+            )
+            with PoolExecutor(max_workers=4) as executor:
+                for _ in executor.map(self.check_url, urls):
+                    pass
